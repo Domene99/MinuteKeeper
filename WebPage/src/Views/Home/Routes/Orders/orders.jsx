42c49ca3a8752cf1cf52c2356f5c2a075ffc66ea
jorgeabdo <a01194743@itesm.mx>
2020-04-15 14:44:47 -0500

    Logo

--- WebPage/src/Views/Home/Routes/Orders/orders.jsx
+++ WebPage/src/Views/Home/Routes/Orders/orders.jsx
@@ -2,72 +2,80 @@ import React, { Component } from "react";
 import "../../Home.css";
 import Button from "@material-ui/core/Button";
 import MicRecorder from "mic-recorder-to-mp3";
+// import { PythonShell } from "python-shell";
 
 const Mp3Recorder = new MicRecorder({ bitRate: 128 });
 
 class Orders extends Component {
-	constructor() {
-		super();
-		this.state = {
-			data: [],
-			isRecording: false,
-			blobURL: "",
-			isBlocked: false,
-		};
-	}
+  constructor() {
+    super();
+    this.state = {
+      data: [],
+      isRecording: false,
+      blobURL: "",
+      isBlocked: false,
+    };
+  }
 
-	componentDidMount() {
-		navigator.getUserMedia(
-			{ audio: true },
-			() => {
-				console.log("Permission Granted");
-				this.setState({ isBlocked: false });
-			},
-			() => {
-				console.log("Permission Denied");
-				this.setState({ isBlocked: true });
-			}
-		);
-	}
+  componentDidMount() {
+    navigator.getUserMedia(
+      { audio: true },
+      () => {
+        console.log("Permission Granted");
+        this.setState({ isBlocked: false });
+      },
+      () => {
+        console.log("Permission Denied");
+        this.setState({ isBlocked: true });
+      }
+    );
+  }
 
-	start = () => {
-		if (this.state.isBlocked) {
-			console.log("Permission Denied");
-		} else {
-			Mp3Recorder.start()
-				.then(() => {
-					this.setState({ isRecording: true });
-				})
-				.catch((e) => console.error(e));
-		}
-	};
+  start = () => {
+    if (this.state.isBlocked) {
+      console.log("Permission Denied");
+    } else {
+      Mp3Recorder.start()
+        .then(() => {
+          this.setState({ isRecording: true });
+        })
+        .catch((e) => console.error(e));
+    }
+  };
+  //   python = (blob) => {
+  //     PythonShell.run("Python.py", null, function (err) {
+  //       if (err) throw err;
+  //       console.log("finished");
+  //     });
+  //   };
 
-	stop = () => {
-		Mp3Recorder.stop()
-			.getMp3()
-			.then(([buffer, blob]) => {
-				const blobURL = URL.createObjectURL(blob);
-				this.setState({ blobURL, isRecording: false });
-				window.open(this.state.blobURL, "_blank");
-			})
-			.catch((e) => console.log(e));
-	};
+  stop = () => {
+    Mp3Recorder.stop()
+      .getMp3()
+      .then(([buffer, blob]) => {
+        const blobURL = URL.createObjectURL(blob);
+        this.setState({ blobURL, isRecording: false });
+        window.open(this.state.blobURL, "_blank");
+        // this.python(blob);
+      })
+      .catch((e) => console.log(e));
+  };
 
-	render() {
-		return (
-			<div className="Dashboard">
-				<div className="Container">
-					<div>
-						<Button onClick={this.start} disabled={this.state.isRecording}>
-							Record
-						</Button>
-						<Button onClick={this.stop} disabled={!this.state.isRecording}>
-							Stop
-						</Button>
-					</div>
-				</div>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="Dashboard">
+        <div className="Container">
+          <div>
+            <Button onClick={this.start} disabled={this.state.isRecording}>
+              Record
+            </Button>
+            <Button onClick={this.stop} disabled={!this.state.isRecording}>
+              Stop
+            </Button>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 export default Orders;
